5153e191ef353bbbe0a88dd03f6af83fdfc585a9
bhuvanesh-1703 <bhuvaneshmahi2003@gmail.com>
2026-03-21 23:12:07 +0530

    feat: Add Product component with product listing, search, sort, and add-to-cart functionality.

--- src/HeaderComponent/Product.jsx
+++ src/HeaderComponent/Product.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "../css/Product.css";
+import "../css/product.css";
 import { useNavigate, Link } from "react-router-dom";
 import Swal from "sweetalert2";
 
